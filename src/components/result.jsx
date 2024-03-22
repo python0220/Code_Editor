8937c1a45db004a055738e17bbbf11ff587b2930
python0220 <pythonking0220@gmail.com>
2024-03-22 14:58:31 +0530

    ready to go

--- src/components/result.jsx
+++ src/components/result.jsx
@@ -4,22 +4,36 @@ import {Box,styled} from '@mui/material';
 import { useContext } from 'react';
 import { DataContext } from '../context/DataProvider';
 
+const Container = styled(Box)`
+    height: 41vh
+`
+
 const Result = () => {
     const {Html,Css,Js} = useContext(DataContext);
-    const {src,setSrc} = useState('');
+    const [src,setSrc] = useState('');
     const srcCode = `
     <html>
     <body>${Html}</body>
     <style>${Css}</style>
-    <sript>${Js}</sript>
-    </html>`
+    <script>${Js}</script>
+    </html>`;
+
+
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrc(srcCode);
+        },1000)
+
+        return () => clearTimeout(timeout);
+    },[Html,Css,Js])
     return (
-        <Box>
+        <Container>
             <iframe title='Output' sandbox='allow-scripts' frameBorder={0}
             width="100%"
             height= "100%"
-             srcDoc={srcCode}></iframe>
-        </Box>
+             srcDoc={src}></iframe>
+        </Container>
     )
 }
 
